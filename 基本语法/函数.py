93a455c5d166123f5078d5dd5704a39c0e06cc9b
Jon Snow <885479297@qq.com>
2020-06-03 18:52:45 +0800

    函数学习

--- 基本语法/函数.py
+++ 基本语法/函数.py
@@ -1,17 +1,31 @@
-# 同一目录
-import function # 引用模块
-print(function.func())
+'''
+    【position, keyword】
+    一、定义函数遵循：定义的先是position argument，后是keyword  argument
+    二、使用函数的默认值： 函数的默认值必须作为最后的参数来定义
+    三、同理，因为函数定义要求带有默认值的参数在后，所以调用函数，keyword argument也应该放在后面
+    否则会报错：例如 def plugin(a=1, b): 应该将a=1放后面，否则会报错
+    【总结：不管是定义函数、调用函数，带有默认值的关键词入参必须放在后面】
+    【不带=在前，带=的在后】
+'''
 
-# 同一目录
-import function as newModule # 对模块重命名
-print(newModule.func())
-
-# 同一目录
-from function import func # 引入function模块中的函数func
-print(func())
-
-# 同一目录
-from function import func as newFunc # 引入function模块中的函数func，并且重命名为newFunc
-print("%s: 引入function模块中的函数func，并且重命名为newFunc"%newFunc())
+# 函数的作用域
+a, b = 520, 250
 
 
+def plugin():
+    print("a的值是：%d" % a)
+
+
+def utils():
+    b = 1314
+    print("b的值是：{}".format(b))
+
+# plugin()
+# utils()
+
+
+# 函数的入参
+def customPlugin(a, b=2, c=3):
+    pass
+
+customPlugin(1, b=2, c=3)
